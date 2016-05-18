a087b09e2e3f41980d39431f0af80f00bac18340
Nathan Havens <nathanhavens@earthlink.net>
2016-05-18 16:37:12 -0400

    added path planner

---
+++ wIRIS/src/obstacle_detection/src/path_planning_module.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+
+#include "checkboard_navigation_module.h"
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+
+using namespace std;
+using namespace cv;
+
+#define pow2(x) (x)*(x)
+
+#define LINEAR_CONST  2
+#define ANGULAR_CONST 0.2
+
+//Global variables
+double goal_x;
+double goal_y;
+
+double forward_cntl;
+double turning_cntl;
+
+void init_path_planning(void* a)
+{
+	while(1)
+	{
+		//Get robot position
+		chesspos pos;
+		pos = get_chessboard_navigation_pos();
+
+		//Message setup
+		forward_cntl = sqrt(pow2(goal_x - pos.x) + pow2(goal_y - pos.y))/LINEAR_CONST;
+		turning_cntl = (atan2(goal_y - pos.y, goal_x - pos.x) - pos.t)/ANGULAR_CONST;
+	}
+}
