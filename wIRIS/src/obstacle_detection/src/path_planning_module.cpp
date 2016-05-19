b1f816121bdd95607e43877990bfbc3c45a5d52e
Andres Rodriguez Reina <andres.r.reina@gmail.com>
2016-05-19 06:48:05 -0500

    remove unnecessary

--- wIRIS/src/obstacle_detection/src/path_planning_module.cpp
+++ wIRIS/src/obstacle_detection/src/path_planning_module.cpp
@@ -89,9 +89,9 @@ void* path_planning(void* unused)
         msg.data = ss.str();
         pub_control.publish(msg);
         
-        sleep(0.050);
+        usleep(50);
         count_loops++;
-        if((count_loops%50000)==1)
+        if((count_loops%20)==1)
         {
             std::cout<<"\033[0;32m"<< "PATHPLAN: current "<<pos.x<<" "<<pos.y<<" "<<pos.t<<" target "<< goal_x << " " << goal_y << " action f "<< forward_cntl << " t " << turning_cntl <<"\033[0m\n";
         }
@@ -123,7 +123,7 @@ void wait_for_dist(double epsilon, const char * comment="")
         dist = sqrt(pow2(goal_x - pos.x) + pow2(goal_y - pos.y));
 
         //Wait for 10 ms
-        sleep(0.01);
+        usleep(10);
     }
 
     control_direction = 0;
