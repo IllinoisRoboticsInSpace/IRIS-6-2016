260c71e298b262f0f3a3b2bbe7173013ae77025f
Nathan Havens <nathanhavens@earthlink.net>
2016-05-19 00:01:21 -0400

    fsm draft

--- wIRIS/src/obstacle_detection/src/main.cpp
+++ wIRIS/src/obstacle_detection/src/main.cpp
@@ -68,13 +68,15 @@ int main(int argc, char **argv)
     pthread_t chessboard_t;
     pthread_t navigation_t;
     pthread_t path_planning_t;
+    pthread_t fsm_t;
 
     
 
     int chessboard = pthread_create(&chessboard_t, NULL, init_chessboard_navigation, (void*)&stop_flag);
     int navigation = pthread_create(&navigation_t, NULL, init_kinect_mapping, (void*)&stop_flag);
     int path = pthread_create(&path_planning_t, NULL, path_planning, 0);
-    if(navigation || chessboard || path_planning)
+    int fsm = pthread_create(&fsm_t, NULL, fsm, 0);
+    if(navigation || chessboard || path_planning || fsm)
         exit(EXIT_FAILURE);
 
     ros::NodeHandle n("main_estimator");
@@ -102,6 +104,7 @@ int main(int argc, char **argv)
     pthread_join(chessboard_t, 0);
     pthread_join(navigation_t, 0);
     pthread_join(path_planning_t, 0);
+    pthread_join(fsm_t, 0);
 
 
 }
