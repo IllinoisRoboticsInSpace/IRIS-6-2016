d1d729f910537ed24515f718ccf9a874a032a988
Andres Rodriguez Reina <andres.r.reina@gmail.com>
2016-05-19 06:40:38 -0500

    remove unnecessary

--- wIRIS/src/obstacle_detection/src/path_planning_module.cpp
+++ wIRIS/src/obstacle_detection/src/path_planning_module.cpp
@@ -90,7 +90,8 @@ void* path_planning(void* unused)
         
         sleep(0.050);
         static int count_loops=0;
-        if((count_loops++%20)==0)std::cout<<"\033[0;32m"<< "PATHPLAN: current "<<pos.x<<" "<<pos.y<<" "<<pos.t<<" target "<< goal_x << " " << goal_y << " action f "<< forward_cntl << " t " << turning_cntl <<"\033[0m\n";
+        count_loops++;
+        if((count_loops%20)==0)std::cout<<"\033[0;32m"<< "PATHPLAN: current "<<pos.x<<" "<<pos.y<<" "<<pos.t<<" target "<< goal_x << " " << goal_y << " action f "<< forward_cntl << " t " << turning_cntl <<"\033[0m\n";
     }
 }
 
